2cb76c91428dbad59e2e743500bf2639eb3fed5e
dafaronbi <>
2021-09-23 02:25:41 -0400

    updated for public broadcasting

--- lsamp_web/settings.py
+++ lsamp_web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5r7^r5bfl^6_47&8jq-8-@l9+95&z^nui%9__03n^s!j3z6&d1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','18.188.196.175']
 
 
 # Application definition
